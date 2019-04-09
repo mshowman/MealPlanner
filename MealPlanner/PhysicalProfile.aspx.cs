1f9d7f3123b87b9cfe0d20ef12f45f8d232f91f8
Matt Showman <mshowman@xogrp.com>
2019-04-08 20:08:01 -0400

    creates physical profile page

---
+++ MealPlanner/PhysicalProfile.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MealPlanner
+{
+    public partial class PhysicalProfile : System.Web.UI.Page
+    {
+        private string showGoals;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void loseWeight_CheckedChanged(object sender, EventArgs e)
+        {
+            showGoals = loseWeight.Enabled ? "block" : "none";
+            lossGoals.Attributes.Add("style", "display: " + showGoals);
+        }
+    }
+}
